0dcde7413ff3eb856d686a4c43b769388ec6cbe0
Vidar Eldøy <vidar@eldoy.com>
2020-01-20 09:51:21 +0100

    Update presang

--- scripts/build.js
+++ scripts/build.js
@@ -4,7 +4,6 @@ const x = require('child_process').execSync
 // Package files
 const pkg = x('pkg --out-path dist .')
 
-// TODO(vidar): Need to make this more solid with versioning in file name
 x('mv dist/waveorb-core-macos dist/server-macos')
 x('mv dist/waveorb-core-win.exe dist/server-win.exe')
 x('mv dist/waveorb-core-linux dist/server-linux')
