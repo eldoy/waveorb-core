53df4bda1e5d2952803ce7acad49e0813365a84b
Vidar Eldøy <vidar@eldoy.com>
2020-01-24 13:02:00 +0100

    Use development instead of dev

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const serve = require('./lib/serve.js')
 const package = require('./package.json')
 console.log(`${package.name} ${package.version}`)
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'dev'
+  process.env.NODE_ENV = 'development'
 }
 (async function() { serve(await loader()) }())
