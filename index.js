dd84d2c6accb9d9bd92daf8e498eda5f08ac6fb9
Vidar Eldøy <vidar@eldoy.com>
2020-01-24 14:59:00 +0100

    Move loader to presang

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const loader = require('./lib/loader.js')
+const { loader } = require('presang')
+const locales = require('./lib/locales.js')
 const serve = require('./lib/serve.js')
 const package = require('./package.json')
 console.log(`${package.name} ${package.version}`)
 if (!process.env.NODE_ENV) {
   process.env.NODE_ENV = 'development'
 }
-(async function() { serve(await loader()) }())
+(async function() { serve(await loader({ locales })) }())
--- test/features/router.test.js
+++ test/features/router.test.js
@@ -1,7 +1,6 @@
-const { i18n } = require('presang')
+const { i18n, loader } = require('presang')
 const router = require('../../lib/router.js')
-const loader = require('../../lib/loader.js')
-const LOCALES = require('../../lib/locales.js')
+const locales = require('../../lib/locales.js')
 
 describe('router', () => {
   it('should be a function', async () => {
@@ -9,49 +8,49 @@ describe('router', () => {
   })
 
   it('should match the route main function', async () => {
-    const app = await loader({ path: 'test/apps/app2' })
+    const app = await loader({ path: 'test/apps/app2', locales })
     const $ = { app, params: { path: 'createProject' } }
     const result = await router($)
     expect(result.hello).toBe('bye')
   })
 
   it('should match the route before function', async () => {
-    const app = await loader({ path: 'test/apps/app3' })
+    const app = await loader({ path: 'test/apps/app3', locales })
     const $ = { app, params: { path: 'createProject' } }
     const result = await router($)
     expect(result.hello).toBe('bye')
   })
 
   it('should match the route after function', async () => {
-    const app = await loader({ path: 'test/apps/app4' })
+    const app = await loader({ path: 'test/apps/app4', locales })
     const $ = { app, params: { path: 'createProject' } }
     const result = await router($)
     expect(result.hello).toBe('bye')
   })
 
   it('should curry function result1', async () => {
-    const app = await loader({ path: 'test/apps/app5' })
+    const app = await loader({ path: 'test/apps/app5', locales })
     const $ = { app, params: { path: 'createProject' } }
     const result = await router($)
     expect(result.hello).toBe('before')
   })
 
   it('should curry function result2', async () => {
-    const app = await loader({ path: 'test/apps/app5' })
+    const app = await loader({ path: 'test/apps/app5', locales })
     const $ = { app, params: { path: 'updateProject' } }
     const result = await router($)
     expect(result.hello).toBe('main')
   })
 
   it('should run filters', async () => {
-    const app = await loader({ path: 'test/apps/app6' })
+    const app = await loader({ path: 'test/apps/app6', locales })
     const $ = { app, params: { path: 'createProject' } }
     const result = await router($)
     expect(result.hello).toBe('bye')
   })
 
   it('should validate data', async () => {
-    const app = await loader({ path: 'test/apps/app7' })
+    const app = await loader({ path: 'test/apps/app7', locales })
     const $ = {
       app,
       params: {
@@ -60,7 +59,7 @@ describe('router', () => {
           name: 'hey'
         }
       },
-      t: i18n.t({ locales: LOCALES })
+      t: i18n.t({ locales })
     }
     let result = await router($)
     expect(result.error.message).toBe('validation failed')
@@ -72,7 +71,7 @@ describe('router', () => {
   })
 
   it('should match request pathname', async () => {
-    const app = await loader({ path: 'test/apps/app7' })
+    const app = await loader({ path: 'test/apps/app7', locales })
     const $ = { app, params: { path: 'createProject'} }
     let result = await router($)
     expect(result.hello).toBe('bye')
